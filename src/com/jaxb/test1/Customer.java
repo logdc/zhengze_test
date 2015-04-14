d292be79b2867d018ea589eddc9aeca90e377519
robinMac <logdc@hotmail.com>
2015-04-14 10:37:03 +0800

    add jaxb samples

---
+++ src/com/jaxb/test1/Customer.java
@@ -0,0 +1,40 @@
+package com.jaxb.test1;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Created by applec on 15-4-14.
+ */
+@XmlRootElement
+public class Customer {
+	private String name;
+	private int age;
+	private int id;
+
+	public String getName() {
+		return name;
+	}
+
+	@XmlElement
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+	@XmlElement
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getId() {
+		return id;
+	}
+	@XmlAttribute
+	public void setId(int id) {
+		this.id = id;
+	}
+}
